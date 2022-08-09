105b429b6454d4a1451a6299b9add6a4c991d056
Nelson C <n.devpro@protonmail.com>
2022-08-09 11:50:49 -0400

    navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,21 +1,78 @@
-import React, { useEffect, useState } from 'react';
-import { BiMenuAltRight } from 'react-icons/bi';
+import React, { useState } from 'react';
+import { BiMenuAltRight, BiX } from 'react-icons/bi';
 
 import cakeIcon from '../../assets/cake-icon.svg';
 import Container from '../Ui/Container/Container';
 import styles from './style.module.css';
 
 const Navbar = () => {
+  const [showNavbar, setShowNavbar] = useState(false);
+  const [showBrandName, setShowBrandName] = useState(false);
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 80) {
+      setShowBrandName(true);
+    } else {
+      setShowBrandName(false);
+    }
+  });
+
   return (
     <header className={`${styles.navbar}`}>
       <Container className={styles.container}>
         <nav className={styles.navbarContent}>
-          <a href="/">
-            <img src={cakeIcon} className={styles.brand} alt="Brand Logo" />
-          </a>
+          <nav className={styles.brand}>
+            <a href="/">
+              <img
+                src={cakeIcon}
+                className={styles.brandImage}
+                alt="Brand Logo"
+              />
+            </a>
+            {/* {showBrandName ? 'Doceria' : null} */}
+            <a
+              href="/"
+              style={{
+                transition: '300ms',
+                // display: `${showBrandName ? 'hidden' : 'block'}`,
+                opacity: !showBrandName ? '0' : '1',
+                color: showBrandName ? '#000' : '#ff1100',
+              }}
+            >
+              Doceria
+            </a>
+          </nav>
 
-          <button type="button" className={styles.menuBtn}>
-            <BiMenuAltRight />
+          <nav
+            className={` ${
+              !showNavbar ? styles.hiddenNavLinks : styles.navLinks
+            } ${styles.navLinks}`}
+          >
+            <ul>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/">Doces</a>
+              </li>
+              <li>
+                <a href="/">Sobre</a>
+              </li>
+              <li>
+                <a href="/">Contato</a>
+              </li>
+              <li>
+                <a href="/">Reviews</a>
+              </li>
+            </ul>
+          </nav>
+
+          <button
+            type="button"
+            className={styles.menuBtn}
+            onClick={() => setShowNavbar(!showNavbar)}
+          >
+            {!showNavbar ? <BiMenuAltRight /> : <BiX />}
           </button>
         </nav>
       </Container>
